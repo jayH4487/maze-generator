aad58084481348961368061e72617c17cebbd3f1
jayH4487 <59086697+jayH4487@users.noreply.github.com>
2020-10-28 17:50:59 +0000

    dfs maze

--- index.js
+++ index.js
@@ -1,12 +1,92 @@
 const getElement = (selector) => document.querySelector(selector)
 
-const $container = getElement(".container")
+const $containerBasic = getElement(".container-basic")
+const $containerDfs = getElement(".container-dfs")
 
 const range = (start, end) => Array.from({length: end - start}, (_, i) => start + i)
 
-const cells = range(0, 100).map(_ => {
+const drawCells = (container, cells) => {
+    container.innerHTML = cells.join("")
+}
+
+const basicCells = range(0, 100).map(_ => {
     const direction = Math.round(Math.random()) ? "forward" : "backward"
-    return `<div class="cell ${direction}"></div>`
+    return `<div class="cell-basic ${direction}"></div>`
 })
 
-$container.innerHTML = cells.join("")
+drawCells($containerBasic, basicCells)
+
+
+// DFS MAZE
+
+const dfsCells = range(0, 100).map(_ => {
+    return []
+    return `<div class="cell-dfs"></div>`
+})
+
+const drawDfsCells = (dfsCells) => {
+    const html = dfsCells.map((directions) => {
+        return `<div class="cell-dfs ${directions.join(" ")}"></div>`
+    }).join("")
+    
+    $containerDfs.innerHTML = html
+}
+
+drawDfsCells(dfsCells)
+
+
+const path = [0, 1, 11]
+
+
+const findFrontiers = (cell) => {
+    const up = cell - 10 < 0 ? -1 : cell - 10
+    const down = cell + 10 > 100 ? -1 : cell + 10
+    const left = Math.floor((cell - 1) / 10) === Math.floor((cell) / 10) ? cell - 1 : -1
+    const right = Math.floor((cell + 1) / 10) === Math.floor((cell) / 10) ? cell + 1 : -1
+
+    return [up, down, left, right].filter(el => el > -1 && el < 100)
+}
+
+const buildPath = (currentCell, endCell, frontier, explored=new Set(), path=[]) => {
+    if (current === end) {
+        return path
+    }
+    const newFrontiers = findFrontiers(currentCell).filter(cell => !explored.has(cell))
+
+}
+
+const removeWall = (current, next) => {
+    const isLeftToRight = Math.floor(current / 10) === Math.floor(next / 10) && current < next
+    const isRightToLeft = Math.floor(current / 10) === Math.floor(next / 10) && current > next
+    const isTopToBottom = (current % 10) === (next % 10) && current < next
+    const isBottomToTop = (current % 10) === (next % 10) && current > next
+
+    if (isLeftToRight) {
+        dfsCells[current] = [...dfsCells[current], "right"]
+        dfsCells[next] = [...dfsCells[next], "left"]
+    }
+    if (isRightToLeft) {
+        dfsCells[current] = [...dfsCells[current], "left"]
+        dfsCells[next] = [...dfsCells[next], "right"]
+    }
+    if (isTopToBottom) {
+        dfsCells[current] = [...dfsCells[current], "bottom"]
+        dfsCells[next] = [...dfsCells[next], "top"]
+    }
+    if (isBottomToTop) {
+        dfsCells[current] = [...dfsCells[current], "top"]
+        dfsCells[next] = [...dfsCells[next], "bottom"]
+    }
+}
+
+const removeWalls = ([current, next, ...rest]) => {
+    if (next === undefined) {
+        return
+    }
+    removeWall(current, next)
+    return removeWalls([next, ...rest])
+}
+
+
+removeWalls(path)
+drawDfsCells(dfsCells)
