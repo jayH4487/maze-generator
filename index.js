9b705acfe1640d77c229b105b12fd967f148f233
jayH4487 <59086697+jayH4487@users.noreply.github.com>
2020-11-06 18:17:55 +0000

    select cell size

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 const getElement = (selector) => document.querySelector(selector)
 
 const $container = getElement(".container")
-const $randomizeMaze = getElement(".btn-randomize")
+const $cellSize = getElement(".cell-size")
+const $randomizeMaze = getElement(".btn")
 
 const range = (start, end) => Array.from({length: end - start}, (_, i) => start + i)
-
-const drawCells = (cells) => {
-    const html = cells.map((directions) => {
-        return `<div class="cell ${directions.join(" ")}"></div>`
-    }).join("")
-    
-    $container.innerHTML = html
-}
 
 const randomizeArray = (arr) => {
     if (arr.length === 0) {
@@ -19,53 +12,77 @@ const randomizeArray = (arr) => {
     }
     const randomIndex = Math.floor(Math.random() * arr.length)
     return [arr[randomIndex], ...randomizeArray([...arr.slice(0, randomIndex), ...arr.slice(randomIndex + 1)])]
-
 }
 
-const getSuccessors = (cell) => {
-    const up = cell - 10 < 0 ? -1 : cell - 10
-    const down = cell + 10 > 100 ? -1 : cell + 10
-    const left = Math.floor((cell - 1) / 10) === Math.floor((cell) / 10) ? cell - 1 : -1
-    const right = Math.floor((cell + 1) / 10) === Math.floor((cell) / 10) ? cell + 1 : -1
+const getSuccessors = (cell, cellSize) => {
+    const width = 400 / cellSize
+    const height = 400 / cellSize
 
-    const validSuccessors = [down, left, right, up].filter(el => el > -1 && el < 100)
+    const up = cell - height < 0
+        ? -1 
+        : cell - height
+
+    const down = cell + height > height * width
+        ? -1
+        : cell + height
+
+    const left = Math.floor((cell - 1) / width) === Math.floor((cell) / width)
+        ? cell - 1
+        : -1
+
+    const right = Math.floor((cell + 1) / width) === Math.floor((cell) / width)
+        ? cell + 1
+        : -1
+
+    const validSuccessors = [down, left, right, up].filter(el => el > -1 && el < width * height)
     return randomizeArray(validSuccessors.map(el => [cell, el]))
 }
 
 const buildPath = (
     currentCell,
-    frontier=getSuccessors(currentCell[1]),
+    cellSize,
+    frontier=getSuccessors(currentCell[1], cellSize),
     explored=new Set([0]),
-    path=[currentCell]
+    path=[currentCell],
 ) => {
     if (frontier.length === 0) {
         return path
     }
-    const successors = getSuccessors(currentCell[1])
+    const successors = getSuccessors(currentCell[1], cellSize)
 
     const [
         updatedCurrentCell,
         ...restFrontier
     ] = [...successors, ...frontier].filter(([_, cell]) => !explored.has(cell))
+
     const updatedExplored = explored.add((updatedCurrentCell || [0, 0])[1])
+
     const updatedPath = [...path, updatedCurrentCell || [0, 0]]
 
-    return buildPath(updatedCurrentCell, restFrontier, updatedExplored, updatedPath)
+    return buildPath(
+        updatedCurrentCell,
+        cellSize,
+        restFrontier, updatedExplored,
+        updatedPath,
+    )
 
 }
 
-const removeWalls = ([currentPath, ...restPath], cells) => {
+const removeWalls = ([currentPath, ...restPath], cells, cellSize) => {
     if (currentPath === undefined) {
         return cells
     }
     const [current, next] = currentPath
+
+    const height = 400 / cellSize
+
     const walls = new Map([
         [1, "right"],
         [-1, "left"],
-        [10, "bottom"],
-        [-10, "top"]
+        [height, "bottom"],
+        [-height, "top"]
     ])
-
+    
     const currentWallToRemove = walls.get(next - current)
     const nextWallToRemove = walls.get(current - next)
 
@@ -82,17 +99,42 @@ const removeWalls = ([currentPath, ...restPath], cells) => {
         return cell
     })
 
-    return removeWalls(restPath, updatedCells)
+    return removeWalls(restPath, updatedCells, cellSize)
 }
 
+const drawCells = (cells, cellSize) => {
+    const html = cells.map((directions) => {
+        return `
+            <div
+                class="cell ${directions.join(" ")}"
+                style="width:${cellSize}px;height:${cellSize}px"
+            >
+            </div>
+        `
+    }).join("")
+    
+    $container.innerHTML = html
+}
 
-const drawMaze = () => {
-    const cells = range(0, 100).map(_ => [])
-    const path = buildPath([0, 0])
-    const updatedCells = removeWalls(path, cells)
-    drawCells(updatedCells)
+const drawMaze = (cellSize=40) => {
+    const cellCount = (400 / cellSize)**2
+    const cells = range(0, cellCount).map(_ => [])
+    const path = buildPath([0, 0], cellSize)
+    const updatedCells = removeWalls(path, cells, cellSize)
+    drawCells(updatedCells, cellSize)
 }
 
 drawMaze()
 
-$randomizeMaze.addEventListener("click", drawMaze)
+const setCellSize = (size) => {
+    const cellSize = {
+        small: 40,
+        medium: 20,
+        large: 10
+    }
+    drawMaze(cellSize[size])
+}
+
+$cellSize.addEventListener("change", (e) => setCellSize(e.target.value))
+
+$randomizeMaze.addEventListener("click", () => setCellSize($cellSize.value))
